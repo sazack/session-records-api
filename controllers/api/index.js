d48f0dc0ef32af5f93eddf53b87f54db18ea2942
sanjeevmgr <sanjeevmagar49@gmail.com>
2017-07-11 15:30:06 +0545

    creating merge conflict

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,8 @@
 
 const mw = require('./middleware')
 const students = require('./students')
-console.log(students,mw)
+console.log(students,mw)            
 
 module.exports = function(app){
   app.use('/students',students);
 }
-
